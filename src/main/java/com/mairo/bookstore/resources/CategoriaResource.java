48be6b42fac209e3f610608c426693c4b59bcdad
Mairon <maironpassos@outlook.com>
2021-09-27 09:22:29 -0300

    Post, delete and put mehtods

--- src/main/java/com/mairo/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/mairo/bookstore/resources/CategoriaResource.java
@@ -1,14 +1,20 @@
 package com.mairo.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mairo.bookstore.domain.Categoria;
 import com.mairo.bookstore.dtos.CategoriaDTO;
@@ -33,5 +39,25 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDTO = list.stream().map(obj-> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj); 
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto){
+		Categoria newObj = service.update(id,objDto);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+	}
+
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		 service.delete(id);
+		 return ResponseEntity.noContent().build();
+	}
+	
 }
 //localhost:8080/categorias/1 -> essse 1 é o pathvariable
--- src/main/java/com/mairo/bookstore/service/CategoriaService.java
+++ src/main/java/com/mairo/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mairo.bookstore.domain.Categoria;
+import com.mairo.bookstore.dtos.CategoriaDTO;
 import com.mairo.bookstore.repositories.CategoriaRepository;
 import com.mairo.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -29,5 +30,23 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repository.findAll();
+	}
+
+
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(obj.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 }
